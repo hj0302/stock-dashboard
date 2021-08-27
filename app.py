e14204ee5d44fde7d8bd76d47876d4ebf8532e11
Hyeonjong Moon <57849967+hj0302@users.noreply.github.com>
2021-08-28 00:37:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def load_stock_price_df():
 stock_info_df = load_stock_info_df()
 stock_price_df = load_stock_price_df()
 
-stock_price_df = stock_price_df[stock_price_df['date'] >= '20150101']
+stock_price_df = stock_price_df[stock_price_df['date'] >= '20180101']
 
 sector_df = pd.merge(stock_price_df, stock_info_df[['stockCode', 'stockName', 'sectorName']], on =['stockCode', 'stockName'])
 
